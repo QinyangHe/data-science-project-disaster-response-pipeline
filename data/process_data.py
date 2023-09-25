069484ebf527d9ee0850dd6af8e5308cbba93bec
Qinyang He <qhe57@wisc.edu>
2023-09-25 14:47:12 -0500

    Finish clean_data() and save_data() functions in process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -11,7 +11,7 @@ def load_data(messages_filepath, categories_filepath):
     categories_filepath: string. Filepath for message category data
 
     Returns:
-    df: the dataframe of message data and categories data combined
+    df: the dataframe of message data and categories data merged
     '''
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
@@ -20,11 +20,52 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
-    pass
+    '''Clean the merged message and category data by building correct features/target
+
+    Args: df: dataframe of merged message and category data
+
+    Returns:
+    cleaned_df: cleaned data
+    '''
+    #split the category string and expand columns
+    categories = df.categories.str.split(";", expand=True) 
+
+    # extract a list of new column names for categories.
+    row = categories.iloc[0,:]
+    category_colnames = [(lambda string : string[:-2])(category) for category in row]
+    categories.columns = category_colnames
+
+    #Iterate through the category columns in df to keep only the last character of each string (the 1 or 0)
+    for column in categories:
+    # set each value to be the last character of the string
+        categories[column] = categories[column].astype("string").str[-1]
+    
+    # convert column from string to numeric
+    categories[column] = categories[column].astype('int')
+    
+    #drop the original categories column in df and replace it with expanded category data
+    df = df.drop('categories', axis=1)
+    df = pd.concat([df, categories], axis = 1)
+
+    # drop duplicates
+    cleaned_df = df.drop_duplicates()
+    return cleaned_df
 
 
 def save_data(df, database_filename):
-    pass  
+    '''Save the cleaned data into a sql database
+
+    Args:
+    df: dataframe to be saved
+    database_filename: string name of the database
+
+    Returns: 
+    None
+    
+    ''' 
+    engine = create_engine('sqlite:///database_filename.db')
+    df.to_sql('messages_cleaned', engine, index=False)
+    return None
 
 
 def main():
